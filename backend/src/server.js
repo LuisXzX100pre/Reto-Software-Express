18129e1cff72a4fa0fff8563edfefedfa9a3bd35
GaelFdzz <gaelandrade272@gmail.com>
2026-09-12 18:21:04 -0500

    feature: configura servidor de Express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,40 +1,6 @@
 require('dotenv').config();
 
-const express = require('express');
-const cors = require('cors');
-const pool = require('./config/db');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// Ruta principal
-app.get('/', (req, res) => {
-    res.json({
-        mensaje: 'Backend del Torneo de Videojuegos funcionando'
-    });
-});
-
-// Probar conexión con MySQL
-app.get('/api/test-db', async (req, res) => {
-    try {
-        const [rows] = await pool.query('SELECT 1 AS conectado');
-
-        res.json({
-            mensaje: 'Conexión con MySQL correcta',
-            resultado: rows
-        });
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            mensaje: 'Error al conectar con MySQL',
-            error: error.message
-        });
-    }
-});
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
